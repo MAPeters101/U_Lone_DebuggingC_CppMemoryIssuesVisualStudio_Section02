5a339035d78eb0da2a21fc27d703002ec679fa76
Mark Peters <mapeters101@earthlink.net>
2024-12-13 14:48:39 -0500

    Work on lesson010.

---
+++ lesson010.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	//int* ptr = new int{};
+	//cout << *ptr << endl;
+	//delete ptr;
+
+	int* ptr = new int[6] {1,2,3,4};
+	for (int i = 0; i < 6; ++i) {
+		cout << ptr[i] << " ";
+	}
+
+	delete[] ptr;
+
+
+	return 0;
+}
+
+*/
