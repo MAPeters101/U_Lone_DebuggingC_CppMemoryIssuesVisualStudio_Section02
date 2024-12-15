d868eb3dd0a1b45098199fb4fbbeb6712c973b84
Mark Peters <mapeters101@earthlink.net>
2024-12-14 21:47:07 -0500

    Work on lesson013.

---
+++ lesson013.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+/*
+int* ptr[5]{};
+int main() {
+
+	try {
+		for (int i = 0; i < 5; ++i) {
+			cout << "Allocating memory #" << i << endl;
+			ptr[i] = new int[INT_MAX/10];
+			cout << "Success #" << i << endl;
+		}
+	}
+	catch (std::exception& ex) {
+		cout << "Exception: " << ex.what() << endl;
+	}
+
+	return 0;
+}
+*/
+
