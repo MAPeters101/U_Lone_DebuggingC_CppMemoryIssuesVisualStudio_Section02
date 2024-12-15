0449819b504483de9f0a995d7ad67bde9d36c00e
Mark Peters <mapeters101@earthlink.net>
2024-12-14 21:35:18 -0500

    Work on lesson012.

---
+++ lesson012.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class Number {
+	int m_Number;
+public:
+	Number(int number);
+	~Number();
+};
+Number* ptr{};
+int main() {
+	ptr = new Number{ 5 };
+	delete ptr;
+
+	return 0;
+}
+
+Number::Number(int number) :m_Number{ number } {
+
+}
+Number::~Number() {
+	// Release any resources here
+}
+*/
