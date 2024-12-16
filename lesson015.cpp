9ee9329c794105025b920cc80972c6c10321f410
Mark Peters <mapeters101@earthlink.net>
2024-12-16 15:38:58 -0500

    Work on lesson015.

---
+++ lesson015.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <new>
+
+
+using namespace std;
+
+/*
+int main() {
+	int* p = new(std::nothrow) int[INT_MAX / 5];
+	if (p == nullptr) {
+		cout << "Failed to allocate memory" << endl;
+		return -1;
+	}
+
+	delete p;
+
+
+	return 0;
+}
+*/
