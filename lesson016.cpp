3c64fa3ba730c182f42870c36746f27182e4cc2d
Mark Peters <mapeters101@earthlink.net>
2024-12-16 16:15:46 -0500

    Enter code for lesson016.cpp per lesson.

--- lesson016.cpp
+++ lesson016.cpp
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <limits.h>
+#include <windows.h>
+#include <exception>
 #include <iostream>
 
 using namespace std;
 
 
 int main() {
+	HINSTANCE hDll = LoadLibraryA(R"(..\..\VBCppDll\Debug|VSCppdll.dll)");
+	if (hDll == NULL) {
+		puts("Could not load dll");
+		return -1;
+	}
+	int** (*pfn)(int, int);
+	pfn = (int** (*)(int, int))GetProcAddress(hDll, (LPCSTR)1);
+	if (pfn == NULL) {
+		puts("Could not locate function");
+		goto EXIT;
+	}
+	try {
+		int rows = 1000;
+		int columns = 1000000;
+		int** pData = pfn(rows, columns);
+		if (pData == NULL) {
+			printf("Data could not be retrieved\n");
+			goto EXIT;
+		}
+		puts("Processing data");
+	}
+	catch (std::exception& ex) {
+		cout << "[main] Caught exception:" << ex.what() << endl;
+	}
 
-
+EXIT:
+	FreeLibrary(hDll);
 
 	return 0;
 }
